1dd43c85b1349808035b6971519242a3768b112f
Jan Oulehla <oulehlajan@gmail.com>
2024-06-01 16:35:18 +0200

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,20 +1,20 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 
-console.log("API Key:", process.env.REACT_APP_MULTIPAGE_FIREBASE_MOVIE_DB_API_KEY);
-console.log("Auth Domain:", process.env.REACT_APP_MULTIPAGE_FIREBASE_MOVIE_DB_AUTH_DOMAIN);
-console.log("Project ID:", process.env.REACT_APP_MULTIPAGE_FIREBASE_MOVIE_DB_PROJECT_ID);
-console.log("Storage Bucket:", process.env.REACT_APP_MULTIPAGE_FIREBASE_MOVIE_DB_STORAGE_BUCKET);
-console.log("Messaging Sender ID:", process.env.REACT_APP_MULTIPAGE_FIREBASE_MOVIE_DB_MESSAGING_SENDER_ID);
-console.log("App ID:", process.env.REACT_APP_MULTIPAGE_FIREBASE_MOVIE_DB_APP_ID);
+console.log("API Key:", process.env.REACT_APP_MULTIPAGE_MOVIE_FIREBASE_DB_API_KEY);
+console.log("Auth Domain:", process.env.REACT_APP_MULTIPAGE_MOVIE_FIREBASE_DB_AUTH_DOMAIN);
+console.log("Project ID:", process.env.REACT_APP_MULTIPAGE_MOVIE_FIREBASE_DB_PROJECT_ID);
+console.log("Storage Bucket:", process.env.REACT_APP_MULTIPAGE_MOVIE_FIREBASE_DB_STORAGE_BUCKET);
+console.log("Messaging Sender ID:", process.env.REACT_APP_MULTIPAGE_MOVIE_FIREBASE_DB_MESSAGING_SENDER_ID);
+console.log("App ID:", process.env.REACT_APP_MULTIPAGE_MOVIE_FIREBASE_DB_APP_ID);
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_MULTIPAGE_FIREBASE_MOVIE_DB_API_KEY,
-    authDomain: process.env.REACT_APP_MULTIPAGE_FIREBASE_MOVIE_DB_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_MULTIPAGE_FIREBASE_MOVIE_DB_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_MULTIPAGE_FIREBASE_MOVIE_DB_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MULTIPAGE_FIREBASE_MOVIE_DB_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_MULTIPAGE_FIREBASE_MOVIE_DB_APP_ID
+    apiKey: process.env.REACT_APP_MULTIPAGE_MOVIE_FIREBASE_DB_API_KEY,
+    authDomain: process.env.REACT_APP_MULTIPAGE_MOVIE_FIREBASE_DB_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_MULTIPAGE_MOVIE_FIREBASE_DB_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_MULTIPAGE_MOVIE_FIREBASE_DB_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MULTIPAGE_MOVIE_FIREBASE_DB_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_MULTIPAGE_MOVIE_FIREBASE_DB_APP_ID
 };
 
 // init
