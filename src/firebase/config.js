14e8ac6b1225195f6667b62ac91afdc54252a328
Jan Oulehla <oulehlajan@gmail.com>
2024-06-01 15:09:16 +0200

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,13 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 
+console.log("API Key:", process.env.MULTIPAGE_FIREBASE_MOVIE_DB_API_KEY);
+console.log("Auth Domain:", process.env.MULTIPAGE_FIREBASE_MOVIE_DB_AUTH_DOMAIN);
+console.log("Project ID:", process.env.MULTIPAGE_FIREBASE_MOVIE_DB_PROJECT_ID);
+console.log("Storage Bucket:", process.env.MULTIPAGE_FIREBASE_MOVIE_DB_STORAGE_BUCKET);
+console.log("Messaging Sender ID:", process.env.MULTIPAGE_FIREBASE_MOVIE_DB_MESSAGING_SENDER_ID);
+console.log("App ID:", process.env.MULTIPAGE_FIREBASE_MOVIE_DB_APP_ID);
+
 const firebaseConfig = {
     apiKey: process.env.MULTIPAGE_FIREBASE_MOVIE_DB_API_KEY,
     authDomain: process.env.MULTIPAGE_FIREBASE_MOVIE_DB_AUTH_DOMAIN,
